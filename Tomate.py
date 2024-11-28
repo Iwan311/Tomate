e537687773e3578415b9ebc9ea6a1909d990b03b
Johann Bergmüller <bergmueller.johann@t-online.de>
2024-11-28 10:25:29 +0100

    knoblauch

--- Tomate.py
+++ Tomate.py
@@ -1,1 +1,24 @@
 print ("Hallo Boss")
+import random
+
+
+def zufaelliges_rezept():
+    rezepte = [
+        "Spaghetti mit Tomatensauce",
+        "Kartoffelsuppe",
+        "Pfannkuchen",
+        "Salat mit Joghurtdressing",
+        "Pizza Margherita",
+        "Lasagne",
+        "Gemüsesuppe",
+        "Schokoladenkuchen"
+    ]
+
+    print("Willkommen im Zufalls-Rezept-Generator!")
+    print("Dein Rezeptvorschlag ist:")
+    print(random.choice(rezepte))
+
+
+# Programm starten
+if __name__ == "__main__":
+    zufaelliges_rezept()
